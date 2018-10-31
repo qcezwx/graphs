9965b4f2f4bc579acb7ac04ad242d7fdc4c04968
qcezwx <golcov.alex@gmail.com>
2018-10-31 18:53:40 +0300

    initial commit

---
+++ backend/src/main/java/com/graphs/controller/CrudController.java
@@ -0,0 +1,18 @@
+package com.graphs.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CrudController {
+
+    public CrudController() {
+    }
+
+    // Get Mapping
+
+    // Post Mapping
+
+    // Put Mapping
+
+    // Update Mapping
+}
