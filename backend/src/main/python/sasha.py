e79b33484871cced0a14ceffa8754d01f52f97d4
Igor <igor.musinov@phystech.edu>
2019-05-10 19:24:25 +0300

    WIP graph
    1. color fix
    2. score

--- backend/src/main/python/sasha.py
+++ backend/src/main/python/sasha.py
@@ -3,7 +3,8 @@ from itertools import chain
 import time
 
 import petya
-#import serj
+
+# import serj
 
 
 METHODS = {
@@ -25,20 +26,22 @@ def color_graph(graph, edges):
 
     colored_graph = graph.copy()
 
-    active_nodes = chain(*edges)
+    active_nodes = set(chain(*edges))
     for node in colored_graph['nodes']:
         node['color'] = 'green' if node['nodeId'] in active_nodes else 'red'
 
     sorted_edges = list(map(sorted, edges))
     for edge in colored_graph['links']:
         edge['color'] = ('blue' if sorted((edge['nodeId1'], edge['nodeId2']))
-                         in sorted_edges else 'black')
+                                   in sorted_edges else 'black')
 
     return colored_graph
 
 
 def sasha(query, context):
-    """
+    '''
+    Computes and colors the graph.
+
     query = {
         'graph': {
             'nodes': [{'nodeId': int, 'weight': float}]
@@ -47,13 +50,14 @@ def sasha(query, context):
         'method': {'name': str, 'params': {}}
     }
 
-    :param query:
-    :return:
-    """
+    :param query: The query contains data about graph
+    :type query: dict
 
-    # query = json.loads(query)
-
-    edges = [(edge['nodeId1'], edge['nodeId2'], edge['weight']) for edge in query['graph']['links']]
+    :return: Colored graph in json
+    '''
+    assert dict is not None
+    edges = [(edge['nodeId1'], edge['nodeId2'], edge['weight']) for edge in
+             query['graph']['links']]
     nodes = {node['nodeId']: node['weight'] for node in query['graph']['nodes']}
     weights = [nodes[idx] for idx in sorted(nodes)]
 
@@ -63,19 +67,21 @@ def sasha(query, context):
     params = query['method'].get('params')
 
     start_time = time.time()
-    result_edges = method(edges, weights, **params)
+    result_edges, score = method(edges, weights, **params)
     method_time = time.time() - start_time
 
     colored_graph = color_graph(query['graph'], result_edges)
 
     result = {
         'graph': colored_graph,
-        'time': method_time
+        'time': method_time,
+        'score': score
     }
     return json.dumps(result)
 
-    # result1 = method(links, weights, **params)
-    # res_tmp = serj.make_new_graph(src_graph, result1, src_graph.vs["weight"])
-    #
-    # res_graph_1 = src_graph.copy()
-    # res_sums = serj.paint_src(res_graph_1, res_tmp)
+
+if __name__ == "__main__":
+    with open('data_easy.json', 'r') as f:
+        graph_json = json.load(f)
+
+    compute_graph(graph_json, None)
